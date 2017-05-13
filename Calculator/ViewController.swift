49089641efe07fa624e5a072e8ce71b69890b8ef
Armando Silveira <armando@mac-pro.hsd1.ga.comcast.net>
2017-05-12 20:33:23 -0400

    - Made the displayed text dynamically change when the number grows in size. - Implemented UITableView, to show past calculations.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     
     
     let calculations: CalculationDelegate = DefaultCalculationDelegate()
     
+    @IBOutlet weak var tableView: UITableView!
 
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -28,12 +29,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calculations.previousExpressions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.item
+        let (expression, result) = calculations.previousExpressions[index]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "paperTapeCell") as! PaperTapeCell
+        cell.customize(expression + " " + result)
+        return cell
+    }
+
+    func reloadTable() {
+        tableView.reloadData()
+        
+        let lastIndex = calculations.previousExpressions.count - 1
+        let indexPath = IndexPath(item: lastIndex, section: 0)
+        if lastIndex > 0 {
+            tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+        }
+    }
+    
+    
+
     
 
     @IBAction func addButtonPressed(_ sender: UIButton) {
         /* ⌘-Clicking on something in Xcode takes you to where that something is defined in code, so ⌘-Click on the plus sign to see its declaration in the Swift standard library:
          @warn_unused_result
          public func +(lhs: Double, rhs: Double) -> Double
+         (-, *, / are also the same)
          */
         
         setOperator("+", withFunction: +)
@@ -54,11 +81,13 @@ class ViewController: UIViewController {
     @IBAction func equalsButtonPressed(_ sender: UIButton) {
         calculations.clearInputAndSave(true)
         resultLabel.text = calculations.resultNumber.roundedString
+        reloadTable()
     }
     
     @IBAction func clearButtonPressed(_ sender: UIButton) {
         calculations.clearInputAndSave(false)
         resultLabel.text = calculations.resultNumber.roundedString
+        reloadTable()
     }
     
     @IBAction func numberButtonPressed(_ sender: UIButton) {
@@ -80,4 +109,31 @@ class ViewController: UIViewController {
 
     
 }
+
+
+class PaperTapeCell : UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    func customize(_ string : String) {
+        label.text = string
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
